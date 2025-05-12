fc260c922886d1e4d6112f652a3f5f525fcd2e13
72.20 <ziad.amr97534@gmail.com>
2025-05-13 00:10:22 +0300

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,29 @@
+const { Events, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: Events.GuildMemberAdd,
+  async execute(member) {
+    const welcomeChannelId = '1345386967784751195'; 
+    const channel = member.guild.channels.cache.get(welcomeChannelId);
+
+    if (!channel) return;
+
+    const welcomeEmbed = new EmbedBuilder()
+      .setColor('#FFA500')
+      .setTitle('👋 اهلا بيك في سيرفر ويك ستوديو')
+      .setDescription(`${member} discord.gg/wicks`)
+      .addFields(
+        { name: 'عدد الاعضاء', value: `${member.guild.memberCount}`, inline: true },
+        { name: 'تاريخ انشاء الحساب', value: `<t:${Math.floor(member.user.createdAt.getTime() / 1000)}:R>`, inline: true },
+        { name: 'تاريخ الانضمام', value: `<t:${Math.floor(Date.now() / 1000)}:R>`, inline: true }
+      )
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .setFooter({ text: 'discord.gg/wicks' })
+      .setTimestamp();
+
+    await channel.send({ 
+      content: `<@${member.id}>`, 
+      embeds: [welcomeEmbed] 
+    });
+  },
+};
